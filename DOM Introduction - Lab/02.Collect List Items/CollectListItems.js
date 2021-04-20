6e6b42573a7d7b0920ea49fbe0f6ede9a8ecd856
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-04-20 14:09:55 +0300

    Updated tasks.

---
+++ DOM Introduction - Lab/02.Collect List Items/CollectListItems.js
@@ -0,0 +1,6 @@
+function extractText() {
+    const liElements = [...document.getElementsByTagName('li')];
+    const elementsText = liElements.map(e => e.textContent);
+
+    document.getElementById('result').value = elementsText.join("\n");
+}
