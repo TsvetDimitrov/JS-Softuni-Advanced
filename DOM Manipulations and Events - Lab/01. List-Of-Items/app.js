2fd80eaed7a7ce2b79fc06d0e928ce6e6287be5b
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-04-23 16:43:23 +0300

    new solutions

--- DOM Manipulations and Events - Lab/01. List-Of-Items/app.js
+++
@@ -1,9 +0,0 @@
-function addItem() {
-    let items = document.getElementById('items');
-    let newItem = document.getElementById('newItemText').value;
-    let toAdd = document.createElement('li');
-    toAdd.textContent = newItem;
-    items.appendChild(toAdd);
-    //Optional
-    document.getElementById('newItemText').value = '';
-}
