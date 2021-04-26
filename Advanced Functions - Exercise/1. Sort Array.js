70669cc18f3fe0dbc5ce5b5f169dc3679262d01f
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-04-26 23:02:15 +0300

    new tasks.

---
+++ Advanced Functions - Exercise/1. Sort Array.js
@@ -0,0 +1,31 @@
+// Write a function that sorts an array with numeric values in ascending or descending order, depending on an argument that is passed to it. 
+// You will receive a numeric array and a string as arguments to the first function in your code. 
+// •	If the second argument is asc, the array should be sorted in ascending order (smallest values first).
+// •	If it is desc, the array should be sorted in descending order (largest first).
+// Input
+// You will receive a numeric array and a string as input parameters. 
+// Output
+// The output should be the sorted array.
+
+function sortArr(arr, str){
+    if(str === "desc"){
+       return arr.sort((a, b) => b - a);
+    }else if(str === "asc"){
+        return arr.sort((a, b) => a - b);
+    }
+
+}
+
+
+sortArr([14, 7, 17, 6, 8], 'desc')
+
+
+//Another solution
+function sortArrWithObj(arr, str){
+    let map = {
+        'desc': (a, b) => b - a,
+        'asc': (a, b) => a - b
+    }
+
+    return arr.sort(map[str]);
+}
---
+++ Advanced Functions - Exercise/5. Functional Sum.js
@@ -0,0 +1,24 @@
+// Write a function that adds a number passed to it to an internal sum and returns itself with its internal sum set to the new value,
+//  so it can be chained in a functional manner. 
+// Input
+// Your function needs to take one numeric argument.
+// Output
+// Your function needs to return itself with an updated context.
+
+
+
+function add(a) {
+    let sum = 0;
+    sum += a;
+
+    function calc(b) {
+        sum += b;
+        return calc;
+    }
+    calc.toString = () => sum;
+    return calc;
+0
+}
+
+console.log(add(1).toString());
+console.log(add(1)(6)(-3).toString())
