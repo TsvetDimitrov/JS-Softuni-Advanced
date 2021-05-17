3b78e2b6dfaab78713b50f24b34d51adb9f6f51e
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-05-17 15:19:56 +0300

    new edit of a task.

--- Exam/Exam - 10.12.2019/01. Christmas Gifts Delivery/app.js
+++ Exam/Exam - 10.12.2019/01. Christmas Gifts Delivery/app.js
@@ -1,108 +1,60 @@
 function solution() {
-    const [listGift, sent, discarded] = document.querySelectorAll('section ul');
-    const input = document.querySelector('input');
-    document.querySelector('button').addEventListener('click', addGift);
+    const input = document.querySelector(`div input`);
+    const addBtn = document.querySelector(`div button`);
+    const sections = document.querySelectorAll(`section ul`);
+    const listGifts = sections[0];
+    const sentGifts = sections[1];
+    const discardedGifts = sections[2];
 
-    function addGift() {
-        const name = input.value;
-        input.value = '';
-
-        let element = e('li', name, 'giftsList');
-        let sendButton = e('button', 'Send', 'sendButton');
-        let discardButton = e('button', 'Discard', 'discardButton');
-
-        element.appendChild(sendButton);
-        element.appendChild(discardButton);
-
-        sendButton.addEventListener('click', () => send(name, element));
-        discardButton.addEventListener('click', () => discard(name, element));
-
-        listGift.appendChild(element);
-
-        sortGift();
-    }
-
-    function send(name, gift) {
-        gift.remove();
-        const element = e('li', name, 'sentGifts');
-        sent.appendChild(element);
-    }
-
-    function discard(name, gift) {
-        gift.remove();
-        const element = e('li', name, 'discardedGifts');
-        discarded.appendChild(element);
-    }
-
-    function sortGift() {
-        Array.from(listGift.children).sort((a, b) => a.textContent.localeCompare(b.textContent)).forEach((g) => {
-            listGift.appendChild(g);
-        })
-    }
-    function e(type, content, addClass) {
-        const result = document.createElement(type);
+    function el(type, content, addClass){
+        let result = document.createElement(type);
         result.textContent = content;
 
-        if (addClass) {
-            result.className = addClass;
-        }
-
-        return result;
-    }
-}
-
-function solution() {
-    const [gifts, sent, discarded] = document.querySelectorAll('section ul');
-    const input = document.querySelector('input');
-    document.querySelector('button').addEventListener('click', addGift);
-
-
-    function addGift(){
-        const name = input.value;
-        input.value = '';
-
-        const element = e('li', name, 'gift');
-        const sendButton = e('button', 'Send', 'sendButton');
-        const discardButton = e('button', 'Discard', 'discardButton');
-        element.appendChild(sendButton);
-        element.appendChild(discardButton);
-
-
-        sendButton.addEventListener('click', () => sendGift(name, element));
-        discardButton.addEventListener('click', () => discardGift(name, element));
-        gifts.appendChild(element);
-
-
-        sortGifts();
-    }
-
-
-
-    function sendGift(name, gift){
-        gift.remove();
-        const element = e('li', name, 'gift');
-        sent.appendChild(element);
-    }
-
-
-    function discardGift(name, gift){
-        gift.remove();
-        const element = e('li', name, 'gift');
-        discarded.appendChild(element);
-    }
-
-
-    function sortGifts(){
-        Array.from(gifts.children).sort((a, b)=> a.textContent.localeCompare(b.textContent)).forEach(g => gifts.appendChild(g));
-    }
-
-
-    function e(type, content, addClass){
-        const result = document.createElement(type);
-        result.textContent = content;
         if(addClass){
             result.className = addClass;
         }
         return result;
+    }
+
+    addBtn.addEventListener('click', listGift);
+
+    function listGift(e){
+        if(input.value === ''){
+            return;
+        }
+        let giftName = input.value;
+        input.value = '';
+        const sendButton = el('button', 'Send', 'sendButton');
+        sendButton.addEventListener('click', () => sendGift(giftName, li));
+        const discardButton = el('button', 'Discard', 'discardButton');
+        discardButton.addEventListener('click', () => discardGift(giftName, li))
+        const li = el('li', giftName);
+
+        li.appendChild(sendButton);
+        li.appendChild(discardButton);
+
+        listGifts.appendChild(li);    
+
+        sortGifts();
+    }
+    
+    
+    function sendGift(input, li){
+        li.remove();
+        const elem = el('li', input, 'sentGifts');
+        sentGifts.appendChild(elem);
+    }
+
+
+    function discardGift(input, li){
+        li.remove();
+        const elem = el('li', input, 'sentGifts');
+        discardedGifts.appendChild(elem);
+    }
+
+    function sortGifts(){
+        Array.from(listGifts.children).sort((a, b) => a.textContent.localeCompare(b.textContent)).forEach((g) => {
+           listGifts.appendChild(g);
+        })
     }
 }
