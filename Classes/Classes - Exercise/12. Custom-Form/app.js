c134805ffad92895966fb5978a10c219b7462269
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-05-21 14:41:44 +0300

    some files are modified and some files are new solutions.

--- Classes/Classes - Exercise/12. Custom-Form/app.js
+++ Classes/Classes - Exercise/12. Custom-Form/app.js
@@ -1,18 +1,62 @@
 let result = (function () {
-
     class Textbox {
-        //TODO
+        constructor(selector, regex) {
+            this.selector = selector;
+            this._elements = $(selector);
+            this._invalidSymbols = regex;
+            this._value = '';
+ 
+            let that = this;
+            $(selector).on('input change', function () {
+                let value = $(this).val();
+                $(that.selector).val(value);
+                that.value = value;
+            });
+        }
+ 
+        get elements() { return this._elements }
+ 
+        get value() { return this._value }
+        set value(v) {
+            this._value = v;
+            $(this.selector).val(v);
+        }
+ 
+        isValid() { return !this._invalidSymbols.test($(this.selector).val()); }
     }
-
+ 
     class Form {
-        //TODO
+        constructor() {
+            this._element = $('<div>').addClass('form');
+            this._textboxes = [];
+            for (let a of arguments) {
+                if (!(a instanceof Textbox))
+                    throw new Error(`Element is not a Textbox`);
+            }
+            for (let a of arguments) {
+                this._element.append($(a.selector));
+                this._textboxes.push(a);
+            }
+        }
+        submit() {
+            for (let tb of this._textboxes) {
+                if (tb.isValid()) {
+                    $(tb.selector).css('border', '2px solid green');
+                } else {
+                    $(tb.selector).css('border', '2px solid red');
+                }
+            }
+        }
+        attach(selector) {
+            $(selector).append(this._element);
+        }
     }
-
+ 
     return {
         Textbox: Textbox,
         Form: Form
     }
-}())
+})();
 
 let Textbox = result.Textbox;
 let Form = result.Form;
--- Prototypes and Inheritance/Prototypes and Inheritance - Exercise/06. Posts.js
+++ Prototypes and Inheritance/Prototypes and Inheritance - Exercise/06. Posts.js
@@ -28,39 +28,38 @@
 // "Views: {postViews}"
 
 
-function posts(){
+function posts() {
 
-    class Post{
-        constructor(title, content){
+    class Post {
+        constructor(title, content) {
             this.title = title;
             this.content = content;
         }
 
-        toString(){
+        toString() {
             return `Post: ${this.title}\nContent: ${this.content}`;
-            
+
         }
     }
 
-
-    class SocialMediaPost extends Post{
-        constructor(title, content, likes, dislikes){
+    class SocialMediaPost extends Post {
+        constructor(title, content, likes, dislikes) {
             super(title, content);
             this.likes = likes;
             this.dislikes = dislikes;
             this.comments = [];
-        }  
+        }
 
 
-        addComment(comment){
+        addComment(comment) {
             this.comments.push(comment);
         }
-        
-        toString(){
+
+        toString() {
             let result = super.toString();
             let rating = `Rating: ${this.likes - this.dislikes}`;
             let comments = this.comments.length > 0 ? `Comments:\n${this.comments.map(comment => ` * ${comment}`).join('\n')}`
-            : '';
+                : '';
 
             return [result, rating, comments].join("\n").trim();
         }
@@ -68,22 +67,22 @@ function posts(){
     }
 
 
-    class BlogPost extends Post{
-        constructor(title, content, views){
+    class BlogPost extends Post {
+        constructor(title, content, views) {
             super(title, content);
             this.views = views;
         }
 
 
-        view(){
+        view() {
             this.views++;
 
             return this;
         }
 
-        toString(){
-            return super.toString() +`\nViews: ${this.views}`;
-            
+        toString() {
+            return super.toString() + `\nViews: ${this.views}`;
+
         }
     }
     return {
