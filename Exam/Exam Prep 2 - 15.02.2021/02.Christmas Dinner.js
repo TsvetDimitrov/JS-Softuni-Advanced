0bab176a70a58ef76664ae56d4bb313a80a4ddb8
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-05-27 19:23:43 +0300

    New solutions.

--- Exam/Exam Prep 2 - 15.02.2021/02.Christmas Dinner.js
+++ Exam/Exam Prep 2 - 15.02.2021/02.Christmas Dinner.js
@@ -58,6 +58,72 @@ class ChristmasDinner {
 }
 
 
+class ChristmasDinner{
+    constructor(budget){
+        this.budget = budget;
+        this.dishes = [];
+        this.products = [];
+        this.guests = {};
+    }
+
+    get budget(){
+        return this._budget;
+    }
+
+    set budget(value){
+        if(value < 0){
+            throw new Error("The budget cannot be a negative number");
+        }
+        this._budget = value;
+    }
+
+    shopping([product, price]){
+        if(price > this.budget){
+            throw new Error("Not enough money to buy this product");
+        }
+        this.products.push(product);
+        this.budget -= price;
+        return `You have successfully bought ${product}!`;
+    }
+
+    recipes({recipeName, productsList}){
+        if(productsList.some((p) => this.products.includes(p)== false)){
+            throw new Error(`We do not have this product`);
+        }else{
+            let obj = {recipeName, productsList};
+            this.dishes.push(obj);
+            return `${recipeName} has been successfully cooked!`;
+        }
+    }
+
+
+    inviteGuests(name, dish){
+        
+        if(this.dishes.some((d) => d.recipeName === dish) == false){
+            throw new Error(`We do not have this dish`);
+        }else if(this.guests.hasOwnProperty(name)){
+            throw new Error("This guest has already been invited");
+        }
+        this.guests[name] = dish;
+
+        return `You have successfully invited ${name}!`;
+    }
+
+    showAttendance(){
+        let result = [];
+
+        Object.entries(this.guests).forEach(([name, dish] )=> {
+            let consistention = this.dishes.find((g) => g.recipeName == dish)
+            result.push(`${name} will eat ${dish}, which consists of ${consistention.productsList.join(', ')}`);
+        });
+
+        return result.join('\n');
+    }
+}
+
+
+
+
 let dinner = new ChristmasDinner(300);
 
 dinner.shopping(['Salt', 1]);
@@ -90,66 +156,3 @@ console.log(dinner.showAttendance());
 
 
 
-// class ChristmasDinner {
-//     constructor(budget) {
-//         this.budget = budget;
-//         this.dishes = [];
-//         this.products = [];
-//         this.guests = {};
-//     }
-
-
-//     set budget(value) {
-//         if (value < 0) {
-//             throw new Error('The budget cannot be a negative number')
-//         }
-//         this._budget = value;
-//     }
-
-//     get budget() {
-//         return this._budget;
-//     }
-
-//     shopping([type, price]) {
-//         if (price > this.budget) {
-//             throw new Error("Not enough money to buy this product");
-//         }
-
-//         this.budget -= price;
-//         this.products.push(type);
-
-//         return `You have successfully bought ${type}!`
-//     }
-
-
-//     recipes({ recipeName, productsList }) {
-//         if (productsList.some(p => this.products.includes(p) == false)) {
-//             throw new Error("We do not have this product")
-//         }
-
-
-//         this.dishes.push({ recipeName, productsList });
-//         return `${recipeName} has been successfully cooked!`
-//     }
-
-//     inviteGuests(name, dish) {
-//         if(this.dishes.some(d => d.recipeName == dish) == false){
-//             throw new Error ("We do not have this dish");
-//         }else if(this.guests.hasOwnProperty(name)){
-//             throw new Error(`This guest has already been invited`);
-//         }
-
-
-//         this.guests[name] = dish;
-
-//         return `You have successfully invited ${name}!`
-//     }
-
-//     showAttendance() {
-//         let result = [];
-//         Object.entries(this.guests).forEach(([guestName, dish]) => {
-//             result.push(`${guestName} will eat ${dish}, which consists of ${this.dishes.find( d=> d.recipeName == dish).productsList.join(', ')}`);
-//         })
-//         return result.join('\n');
-//     }
-// }
