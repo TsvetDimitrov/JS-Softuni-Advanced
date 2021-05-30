0c06802ebd9e8f964a68501d8848f4bc66b110e9
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-05-30 21:09:41 +0300

    Uploading new solution.

--- JS-Softuni-Applications/EXERCISE ASYNCHRONOUS PROGRAMMING/02.Bus-Schedule/app.js
+++ JS-Softuni-Applications/EXERCISE ASYNCHRONOUS PROGRAMMING/02.Bus-Schedule/app.js
@@ -1,11 +1,26 @@
 function solve() {
-
-    function depart() {
-        console.log('Depart TODO...');
+    const span = document.querySelector('span[class="info"]');
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+    console.log(span);
+    let stop = {
+        next: 'depot'
+    }
+    async function depart() {
+        let url = `http://localhost:3030/jsonstore/bus/schedule/` + stop.next;
+        const response = await fetch(url);
+        const data = await response.json();
+        
+        stop = data;
+        span.textContent = `Next stop ${stop.name}`;
+        departBtn.disabled = true;
+        arriveBtn.disabled = false;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        span.textContent = `Arriving at ${stop.name}`;
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
