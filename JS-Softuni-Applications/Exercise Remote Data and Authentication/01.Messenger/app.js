72e898dccf2bf3f9b86613248413eab814ae5004
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-06-03 14:54:42 +0300

    new solution.

--- JS-Softuni-Applications/Exercise Remote Data and Authentication/01.Messenger/app.js
+++ JS-Softuni-Applications/Exercise Remote Data and Authentication/01.Messenger/app.js
@@ -1,5 +1,34 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('submit').addEventListener('click', () => {
+        const author = document.querySelector('input[name="author"]').value;
+        const content = document.querySelector('input[name="content"]').value;
+
+        sendMessage({author, content});
+        document.querySelector('input[name="author"]').value =  '';
+        document.querySelector('input[name="content"]').value =  '';
+    });
+
+    document.getElementById('refresh').addEventListener('click', getMessages)
+    getMessages()
 }
 
 attachEvents();
+
+
+async function getMessages(){
+    const response = await fetch('http://localhost:3030/jsonstore/messenger');
+    const data = await response.json();
+    document.getElementById('messages').value = Object.values(data).map(x => `${x.author}: ${x.content}`).join('\n');
+}
+
+
+
+async function sendMessage(message){
+    const response = await fetch('http://localhost:3030/jsonstore/messenger', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(message)
+    });
+
+    const data = await response.json();
+}
