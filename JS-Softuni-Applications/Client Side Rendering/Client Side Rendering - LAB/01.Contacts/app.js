ac31c997d0c411c61eae4af92695caaec59e43c5
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-06-09 20:18:55 +0300

    Made changes and text allignment.

--- JS-Softuni-Applications/Client Side Rendering/Client Side Rendering - LAB/01.Contacts/app.js
+++ JS-Softuni-Applications/Client Side Rendering/Client Side Rendering - LAB/01.Contacts/app.js
@@ -1,24 +1,6 @@
-import {html, render} from '../node_modules/lit-html/lit-html.js';
+import {render} from '../node_modules/lit-html/lit-html.js';
 import {contacts} from './contacts.js';
-
-
-
-const cardTemplate = (data) =>html `
-<div class="contact card">
-    <div>
-        <i class="far fa-user-circle gravatar"></i>
-    </div>
-    <div class="info">
-        <h2>Name: ${data.name}</h2>
-        <button @click=${onclick} class="detailsBtn">Details</button>
-        <div class="details" id="${data.id}">
-            <p>Phone number: ${data.phoneNumber}</p>
-            <p>Email: ${data.email}</p>
-        </div>
-    </div>
-</div>
-`;
-
+import cardTemplate from './card.js';
 
 
 const container = document.getElementById('contacts');
@@ -28,13 +10,3 @@ const result = contacts.map(cardTemplate) //.forEach(el => container.appendChild
 render(result, container);
 
 
-function onclick(ev){
-    const element = ev.target.parentNode;
-    const currentStyle = element.querySelector('.details').style.display;
-    if(currentStyle === 'block'){
-        element.querySelector('.details').style.display = 'none';
-    }else{
-        element.querySelector('.details').style.display = 'block';
-    }
-    
-}
