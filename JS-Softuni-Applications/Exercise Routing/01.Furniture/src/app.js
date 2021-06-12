dea38b3db669a9fdbb3998cef54e93165c4a04ea
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-06-12 20:53:28 +0300

    new solutions

--- JS-Softuni-Applications/Exercise Routing/01.Furniture/src/app.js
+++ JS-Softuni-Applications/Exercise Routing/01.Furniture/src/app.js
@@ -1,4 +1,5 @@
 import page from '../node_modules/page/page.mjs';
+import { render } from '../node_modules/lit-html/lit-html.js'
 
 import { createPage } from './views/create.js';
 import { loginPage } from './views/login.js';
@@ -11,15 +12,22 @@ import { detailsPage } from './views/details.js';
 import * as api from './api/data.js';
 
 window.api = api;
+const main = document.querySelector('.container');
 
-
-page('/', dashboardPage);
-page('/dashboard', dashboardPage);
-page('/my-furniture', myPage);
-page('/details/:id', detailsPage);
-page('/create', createPage);
-page('/edit/:id', editPage);
-page('/register', registerPage);
-page('/login', loginPage);
+page('/', renderMiddleware, dashboardPage);
+page('/dashboard', renderMiddleware, dashboardPage);
+page('/my-furniture', renderMiddleware, myPage);
+page('/details/:id', renderMiddleware, detailsPage);
+page('/create', renderMiddleware, createPage);
+page('/edit/:id', renderMiddleware, editPage);
+page('/register', renderMiddleware, registerPage);
+page('/login', renderMiddleware, loginPage);
 
 page.start();
+
+
+
+function renderMiddleware(ctx, next) {
+    ctx.render = (content) => render(content, main);
+    next();
+}
