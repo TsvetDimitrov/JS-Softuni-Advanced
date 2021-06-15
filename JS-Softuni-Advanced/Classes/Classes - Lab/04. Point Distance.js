8e4e8f33e7b14e18d49c1b37e55a3853e2d61f68
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-06-16 02:00:17 +0300

    new classes - lab solution.

--- JS-Softuni-Advanced/Classes/Classes - Lab/04. Point Distance.js
+++ JS-Softuni-Advanced/Classes/Classes - Lab/04. Point Distance.js
@@ -5,3 +5,18 @@
 // Output
 // The distance() method should return a number, the distance between the two point parameters.
 // Submit the class definition as is, without wrapping it in any function.
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+
+    }
+
+    static distance(a, b) {
+        if (a instanceof Point == false || b instanceof Point == false) {
+            throw new TypeError('Parameter must be of type Point');
+        }
+
+        return Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2);
+    }
+}
