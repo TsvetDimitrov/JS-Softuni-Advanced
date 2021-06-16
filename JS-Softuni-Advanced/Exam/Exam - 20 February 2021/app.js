d280f411cda3a7f8f8f45a14907d93eeb08d8aae
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-06-16 22:22:31 +0300

    solution №2 of softBlog

--- JS-Softuni-Advanced/Exam/Exam - 20 February 2021/app.js
+++ JS-Softuni-Advanced/Exam/Exam - 20 February 2021/app.js
@@ -1,12 +1,10 @@
 function solve() {
-   let olState = [];
    document.querySelector('.btn').addEventListener('click', (e) => {
       e.preventDefault();
       const author = document.getElementById('creator');
       const title = document.getElementById('title');
       const category = document.getElementById('category');
       const content = document.getElementById('content');
-
 
       // I have been 2 hours on this task because of this stupid. IF statement and if you are reading this, 
       // please read your taks description carefully and do not improvise
@@ -72,3 +70,70 @@ function solve() {
 }
 
 
+//Another solution.
+//If you uncomment this solution it will ovewrite the previous one. 
+//document is a property of window, so i can use this: 
+
+/*function solve() {
+   const inputFiels = Array.from(window.document.querySelectorAll('p input'));
+   const author = inputFiels[0];
+   const title = inputFiels[1];
+   const category = inputFiels[2];
+   const content = window.document.getElementById('content');
+   const section = window.document.querySelector('main > section');
+
+   window.document.querySelector('.btn').addEventListener('click', appendText);
+
+   function appendText(e){
+      e.preventDefault();
+
+      const article = el('article');
+      // We take the input values after the function invocation 
+      const h1 = el('h1', title.value);
+      const p = el('p', 'Category:');
+      const strong = el('strong', category.value);
+      p.appendChild(strong);
+      const p2 = el('p', 'Creator:');
+      const strong2 = el('strong', author.value);
+      p2.appendChild(strong2);
+      const p3 = el('p', content.value);
+      const div = el('div', undefined, 'buttons');
+      const delBtn = el('button', 'Delete', 'btn delete');
+      delBtn.addEventListener('click', del);
+      const archiveBtn = el('button', 'Archive', 'btn archive');
+      archiveBtn.addEventListener('click', archive);
+      div.appendChild(delBtn);
+      div.appendChild(archiveBtn);
+      article.appendChild(h1);
+      article.appendChild(p);
+      article.appendChild(p2);
+      article.appendChild(p3);
+      article.appendChild(div);
+      section.appendChild(article);
+   }
+
+   function del(e){
+      e.target.parentNode.parentNode.remove();
+   }
+
+   function archive(e){
+      const section = e.target.parentNode.parentNode;
+      const textToAppend = section.querySelector('h1').textContent;  
+      const ol = document.querySelector('ol');
+      let li = el('li', textToAppend);
+      ol.appendChild(li);
+      Array.from(ol.querySelectorAll('li')).sort((a, b) => a.textContent.localeCompare(b.textContent)).forEach((e) => ol.appendChild(e));
+      section.remove();
+   }
+   //This is standart create element function.
+   function el(type, content, addClass) {
+      const result = document.createElement(type);
+      result.textContent = content;
+
+      if (addClass) {
+         result.className = addClass;
+      }
+      return result;
+   }
+}
+*/
