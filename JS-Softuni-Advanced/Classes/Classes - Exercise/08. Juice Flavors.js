fe88a0f60a1b9912c53a95bfcd67bcc0bd689799
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-06-17 16:44:15 +0300

    08 juice flavors solution from classes exercise.

---
+++ JS-Softuni-Advanced/Classes/Classes - Exercise/08. Juice Flavors.js
@@ -0,0 +1,36 @@
+function juiceFlavors(array) {
+    const bottles = new Map();
+    array.reduce((acc, value) => {
+        let [fruit, quantity] = value.split(' => ');
+        quantity = Number(quantity);
+        if (!acc.hasOwnProperty(fruit)) {
+            acc[fruit] = 0;
+        }
+        acc[fruit] += quantity;
+
+        if (acc[fruit] >= 1000) {
+            if (!bottles.has(fruit)) {
+                bottles.set(fruit, 0);
+            }
+            bottles.set(fruit, bottles.get(fruit) + parseInt(acc[fruit] / 1000));
+            acc[fruit] %= 1000;
+        }
+        return acc
+    }, {});
+
+    for (const [fruit, quantity] of bottles) {
+        console.log(`${fruit} => ${quantity}`);
+    }
+}
+// 2000
+// 2032
+// 450
+// 549
+
+juiceFlavors(['Orange => 2000',
+    'Peach => 1432',
+    'Banana => 450',
+    'Peach => 600',
+    'Strawberry => 549']
+
+);
