ef8e8322597446256ef2c4718e88284f62543515
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-06-29 14:25:19 +0300

    login page created

--- JS-Softuni-Applications/Exam Preps/Exam Prep1/Meme_Lounge/src/app.js
+++ JS-Softuni-Applications/Exam Preps/Exam Prep1/Meme_Lounge/src/app.js
@@ -1,17 +1,35 @@
-import page from '../node_modules/page/page.mjs';
 import { render } from '../node_modules/lit-html/lit-html.js';
+import page from '../node_modules/page/page.mjs';
 
 
 
 import { homePage } from './views/home.js';
+import { loginPage } from './views/login.js';
 
 const main = document.querySelector('main');
+setUserNav();
 
 page('/', decorateContext, homePage);
+page('/login', decorateContext, loginPage);
 
 page.start();
 
 function decorateContext(ctx, next) {
     ctx.render = (content) => render(content, main);
+    ctx.setUserNav = setUserNav;
     next();
+}
+
+
+//will read from the sessionStorage
+function setUserNav() {
+    const token = sessionStorage.getItem('authToken');
+
+    if (token != null) {
+        document.querySelector('.user').style.display = '';
+        document.querySelector('.guest').style.display = 'none';
+    } else {
+        document.querySelector('.user').style.display = 'none';
+        document.querySelector('.guest').style.display = '';
+    }
 }
