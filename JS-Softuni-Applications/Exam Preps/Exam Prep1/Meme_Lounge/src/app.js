04e9b1ac967f231c24dcf27e6ae37172804eb431
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-06-29 13:16:33 +0300

    home view made

--- JS-Softuni-Applications/Exam Preps/Exam Prep1/Meme_Lounge/src/app.js
+++ JS-Softuni-Applications/Exam Preps/Exam Prep1/Meme_Lounge/src/app.js
@@ -1,2 +1,17 @@
 import page from '../node_modules/page/page.mjs';
-import render from '../node_modules/lit-html/lit-html.jsl';
+import { render } from '../node_modules/lit-html/lit-html.js';
+
+
+
+import { homePage } from './views/home.js';
+
+const main = document.querySelector('main');
+
+page('/', decorateContext, homePage);
+
+page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, main);
+    next();
+}
