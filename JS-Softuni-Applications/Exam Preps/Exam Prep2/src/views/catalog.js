5304ef37c7a83d554182d1087b01c8b4c2232490
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-07-01 01:07:28 +0300

    pagination done

--- JS-Softuni-Applications/Exam Preps/Exam Prep2/src/views/catalog.js
+++ JS-Softuni-Applications/Exam Preps/Exam Prep2/src/views/catalog.js
@@ -1,14 +1,19 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 
 
-import { getAllListings, getCollectionSize} from '../api/data.js';
+import { getAllListings, getCollectionSize } from '../api/data.js';
 import { carTemplate } from './common/car.js';
 
 const catalogTemplate = (cars, page, pages) => html`<section id="car-listings">
     <h1>Car Listings</h1>
     <div class="listings">
+    <!-- FOR PAGINATION -->
+        <div>Page ${page} / ${pages}
+            ${page > 1? html`<a class="button-list" href="/all-listings?page=${page - 1}">&lt; Prev</a>` : ''}
+            ${page < pages ? html`<a class="button-list" href="/all-listings?page=${page + 1}">Next &gt;</a>` : ''}
+        </div>
 
-    <div>Page ${page} / ${pages}</div>
+    <!-- End Pagination -->
         ${cars.length == 0 ? html`<p class="no-cars">No cars in database.</p>` :
         cars.map(carTemplate)}
 
@@ -20,8 +25,8 @@ const catalogTemplate = (cars, page, pages) => html`<section id="car-listings">
 export async function catalogPage(ctx) {
     const page = Number(ctx.querystring.split('=')[1]) || 1;
     const cars = await getAllListings();
-   const count = await getCollectionSize();
-   const pages = Math.ceil(count / 3);
+    const count = await getCollectionSize();
+    const pages = Math.ceil(count / 3);
     ctx.render(catalogTemplate(cars, page, pages));
 }
 
